376e358602f399521ae95c4e2000bb36c9b7ab40
LoHhhha <1185604794@qq.com>
2024-03-06 23:01:48 +0800

    new file:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define pll pair<long long,long long>
+#define OPTIO std::ios::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0)
+#define INF 0x3f3f3f3f3f3f3f3f
+#define inf 0x3f3f3f3f
+#define MOD int(1e9+7)
+#define N2E5 int(2e5)
+#define MOD1 998244353
+using namespace std;
+
+void init(){
+
+}
+
+void solve(ll& USELESS) {
+    
+}
+
+bool solveYN(ll& USELESS) {
+	return true;
+}
+
+int main() {
+	OPTIO;
+	init();
+	ll t;
+	//cin >> t;
+	t = 1;
+	while (t--)solve(t);
+	//while (t--) {
+	//	if (solveYN(t))cout << "YES" << endl;
+	//	else cout << "NO" << endl;
+	//}
+	return 0;
+}
