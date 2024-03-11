f68ff9663311a602e9b39e939775e80d9206aadd
Lucas <1648136557@qq.com>
2024-03-12 00:10:48 +0800

    add something

--- main.cpp
+++ main.cpp
@@ -1,42 +1,55 @@
-#include<bits/stdc++.h>
-#define endl "\n"
-#define ll long long
-#define ld long double
-#define ull unsigned long long
-#define pll pair<long long,long long>
+// #include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+
+// #define int ll
+#define endl "\n"	// 是否保留待定
 #define OPTIO std::ios::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0)
 #define INF 0x3f3f3f3f3f3f3f3f
 #define inf 0x3f3f3f3f
-#define MOD int(1e9+7)
-#define N2E5 int(2e5)
-#define MOD1 998244353
-using namespace std;
+
+#define DE_BUG
+#ifdef DE_BUG
+    #include "DEBUG.h"
+    using namespace DEBUG_;
+#endif
 
 
-void init(){
+struct Robot {
+	int x, y;	// 机器人当前坐标
+	int goods;	// 0: 无货物 其他整数: 货物编号
+	int status;	// 0: 恢复状态 1: 正常运行状态
+};
+vector<Robot> robot(10);	// 机器人 vector
+
+
+struct Berth {
+	int x, y;			// 码头左上角坐标
+	int transport_time;	// 运输到虚拟点的时间
+	int loading_speed;	// 装载速度: 个/帧
+};
+vector<Berth> berth(10);	// 码头 vector
+
+
+void init() {
 
 }
 
-
-void solve(ll& USELESS) {
-    
-}
-
-bool solveYN(ll& USELESS) {
-	return true;
-}
 
 int main() {
+	#ifdef DE_BUG
+		// 用 cerr 输出调试信息
+		OUTPUT = &std::cerr;
+		debug(1)
+	#endif
+
 	OPTIO;
 	init();
-	ll t;
-	//cin >> t;
-	t = 1;
-	while (t--)solve(t);
-	//while (t--) {
-	//	if (solveYN(t))cout << "YES" << endl;
-	//	else cout << "NO" << endl;
-	//}
+
 	return 0;
 }
 
