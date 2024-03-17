82332ff421c8b9f21adf9e58aff6add661a0d7fe
LoHhhha <1185604794@qq.com>
2024-03-17 23:18:56 +0800

    Co-authored-by: SleepingLucas <SleepingLucas@users.noreply.github.com>

---
+++ Util.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Param.hpp"
+#include "Message.hpp"
+#include "Robot.hpp"
+#include "Packet.hpp"
+#include "Boat.hpp"
+#include "Berth.hpp"
+
+
+// init
+
+void get_robot_can_go();
+void choose_best_berth(int num);
+
